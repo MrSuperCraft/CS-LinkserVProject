cd497b49a33825e40a35ef170886e08a8d851a3b
mrsupercraft <itamarha2006@gmail.com>
2024-04-21 14:49:42 +0300

    update loading screen management for new users

--- scripts/fetchTrack.js
+++ scripts/fetchTrack.js
@@ -35,14 +35,17 @@ async function fetchData(url) {
 }
 
 // Function to handle fetch completion
+// Function to handle fetch completion
 function handleFetchCompletion() {
-    completedFetches++;
-    console.log('Current amount of completed fetches:', completedFetches);
+    if (!allFetchesCompleted) { // Check if all fetches are not yet completed
+        completedFetches++;
+        console.log('Current amount of completed fetches:', completedFetches);
 
-    // Check if all fetches are completed
-    if (completedFetches === totalFetches) {
-        allFetchesCompleted = true;
-        checkLoadingState();
+        // Check if all fetches for new users or user pages without content are completed
+        if (completedFetches === totalFetches - 1) { // Adjusted condition for 3 fetches
+            allFetchesCompleted = true;
+            checkLoadingState();
+        }
     }
 }
 
