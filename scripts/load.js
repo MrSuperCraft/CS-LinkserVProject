21199617362d9d86a47445baa360172f7acabe71
mrsupercraft <itamarha2006@gmail.com>
2024-05-08 13:40:50 +0300

    page load fix for complex usernames

--- scripts/load.js
+++ scripts/load.js
@@ -2,8 +2,11 @@ async function fetchUserIdFromUrl() {
     // Get the current path of the URL, and find the username from there
     const user_name = getUsernameFromURL();
 
+    // Decode the username to replace special characters like "@" and "_" with their original form
+    const decodedUsername = decodeURIComponent(user_name);
+
     try {
-        const response = await fetch(`/api/getUserId?username=${user_name}`);
+        const response = await fetch(`/api/getUserId?username=${decodedUsername}`);
         if (response.ok) {
             const data = await response.json();
             return data.user_id; // Return the user_id from the response data
@@ -16,6 +19,8 @@ async function fetchUserIdFromUrl() {
         return null;
     }
 }
+
+
 
 async function getUserIdWithFallback() {
     // Attempt to fetch user_id from URL
@@ -36,10 +41,10 @@ function getUsernameFromURL() {
 
     if (pathSegments.length === 3 && pathSegments[1] === 'design') {
         // Case 1: URL is /design/${username}
-        username = pathSegments[2];
+        username = pathSegments[2].replace(/_/g, ' ');
     } else if (pathSegments.length === 2 && pathSegments[1] !== '') {
         // Case 2: URL is /${username}
-        username = pathSegments[1];
+        username = pathSegments[1].replace(/_/g, ' ');
     } else {
         // Default case or invalid URL structure
         username = 'Guest';
@@ -53,7 +58,24 @@ function getUsernameFromURL() {
 document.addEventListener('DOMContentLoaded', function () {
     const username = getUsernameFromURL();
 
-    document.getElementById('dynamicUsername').innerText = username;
-    document.getElementById('dynamic-nav-text').innerText = username;
+    const decodedUsername = decodeURI(username);
+    const formattedUsername = decodedUsername.replace(/%40/g, '@'); // Replace %40 with @
+
+
+    document.getElementById('dynamicUsername').innerText = formattedUsername;
+    document.getElementById('dynamic-nav-text').innerText = formattedUsername;
+
+
+    // Get the current URL from window.location
+    const currentURL = window.location.href;
+
+    // Decode the URL to remove encoding
+    const decodedURL = decodeURI(currentURL);
+
+    // Replace spaces with underscores and other special characters as needed
+    const formattedURL = decodedURL.replace(/ /g, '_').replace(/@/g, '@');
+
+    // Replace the URL in the browser's address bar without reloading the page
+    history.replaceState(null, '', formattedURL);
 });
 
